e5cc727a5838061ae3c58758eba2f396d41b293c
youngeunchoi-814 <yec9125@naver.com>
2021-10-13 17:13:08 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -17,11 +17,18 @@ navbarMenu.addEventListener('click',(event)=>{
 
 const target= event.target;
 const link = target.dataset.link;
-if(link==null){
+    if(link==null){
     return;
 }
-console.log(event.target.dataset.link);
-const scrollTo = document.querySelector(link);
-scrollTo.scrollIntoView({behavior: "smooth"});
+    scrollIntoView(link);
 });
 
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click',()=>{
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
