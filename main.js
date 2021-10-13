8f1b9890a9acf06d4952e7ec8952e710e03c6eb9
youngeunchoi-814 <yec9125@naver.com>
2021-10-13 16:53:28 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -4,8 +4,6 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll',()=>{
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY>navbarHeight){
         navbar.classList.add('navbar--dark');
     }else {
@@ -13,3 +11,17 @@ document.addEventListener('scroll',()=>{
     }
     
 });
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event)=>{
+
+const target= event.target;
+const link = target.dataset.link;
+if(link==null){
+    return;
+}
+console.log(event.target.dataset.link);
+const scrollTo = document.querySelector(link);
+scrollTo.scrollIntoView({behavior: "smooth"});
+});
+
