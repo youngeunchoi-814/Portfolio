1760487292eadceb567cf52696026962ebe29371
youngeunchoi-814 <yec9125@naver.com>
2021-10-13 18:03:24 +0900

    JS: Navbar menu toggle

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ document.addEventListener('scroll',()=>{
     
 });
 
+
+
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click',(event)=>{
 
@@ -20,8 +22,15 @@ const link = target.dataset.link;
     if(link==null){
     return;
 }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
+
+//Navbar toggle button
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click',()=>{
+    navbarMenu.classList.toggle('open');
+})
 
 //Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
